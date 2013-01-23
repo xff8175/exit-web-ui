3f7f03a95043d580dc2f938cc2ea0db120fba138
exitsoft <es_vincent@163.com>
2013-01-23 22:12:14 +0800

    对一个基础的widget写一个父类,初始化属性.

--- ui/basic/jquery.ui.basic.js
+++ ui/basic/jquery.ui.basic.js
@@ -1,4 +1,41 @@
 
 (function( $, undefined ) {
-
+	
+	$.widget("ui.basic", {
+		
+		options:{
+			focusable:true,
+			role:'exit-cmp',
+		},
+		
+		basicAttr:function(element) {
+			
+			this.currentWiget = element;
+			this.currentWiget.uniqueId();
+			this.currentWiget.attr("role",this.getRole());
+			
+			if ($.isNotEmpty(this.options.tabIndex) && $.isNumeric(this.options.tabIndex)) {
+				this.currentWiget.attr("tab-index",this.options.tabIndex);
+			}
+			
+		},
+		
+		widget: function() {
+			return this.currentWiget;
+		},
+		
+		getId:function() {
+			return this.basicTarget.id;
+		},
+		
+		setId:function(id) {
+			this.basicTarget.attr("id",id);
+		},
+		
+		getRole:function() {
+			return this.options.role;
+		}
+		
+	});
+	
 })( jQuery );
